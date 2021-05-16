63212ec04aaa51dd560cd9bb90f3a6c7dca5d58f
gustavocbjunior <gustavobarbosaa@gmail.com>
2021-05-15 23:58:07 -0300

    Criando uma tabela urilizando Generation schematics do Angular

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -26,6 +26,10 @@ import { ProductCreateComponent } from './components/product/product-create/prod
 import { RedDirective } from './directives/red.directive';
 import { ForDirective } from './directives/for.directive';
 import { ProductReadComponent } from './components/product/product-read/product-read.component';
+import { ProductRead2Component } from './components/product/product-read2/product-read2.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [
@@ -38,7 +42,8 @@ import { ProductReadComponent } from './components/product/product-read/product-
     ProductCreateComponent,
     RedDirective,
     ForDirective,
-    ProductReadComponent
+    ProductReadComponent,
+    ProductRead2Component
   ],
   imports: [
     BrowserModule,
@@ -53,7 +58,10 @@ import { ProductReadComponent } from './components/product/product-read/product-
     HttpClientModule,
     FormsModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
